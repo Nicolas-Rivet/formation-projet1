b370f1a61bfc18b81fa7242c6a6e27b74790f8fc
Nicolas-Rivet <nicolas@7ici.fr>
2021-06-03 19:56:32 +0200

    Initialisation

---
+++ frontend/js/index.js
@@ -0,0 +1,26 @@
+function showProducts() {
+    let listeProducts='';
+
+    fetch('http://localhost:3000/api/teddies/')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function (produits){
+        for (let i = 0; i < produits.length; i++) {
+            console.log(produits[i]);
+            listeProducts +=    `<div class="col-lg-3 col-md-6 mb-4">
+                                    <div class="card">
+                                        <a href="/teddie/${produits[i]._id}">
+                                            <img src="${produits[i].imageUrl}" class="card-img-top">                                    
+                                        </a>
+                                        <div class="card-body text-center">
+                                            <h5 class=""><strong><a href="/teddie/${produits[i]._id}" class="">${produits[i].name}</a></strong></h5>
+                                            <h4 class=""><strong>${produits[i].price / 100}</strong>&nbsp;€</h4>
+                                        </div>
+                                    </div>
+                                </div>`;
+        }
+        document.getElementById('listeProducts').innerHTML = listeProducts;
+    });
+}
+showProducts();
